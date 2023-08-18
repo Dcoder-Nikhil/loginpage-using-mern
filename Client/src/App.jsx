cbd91c60df0e9bf523d8b4adf99b72027aefe5ca
DcoderLLC <dcoderllc@gmail.com>
2023-08-19 03:29:24 +0530

    added the home section

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -3,7 +3,7 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import Signup from './signup'
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import Login from './login'
-
+import Home from './Home'
 
 function App() {
   const [, ] = useState(0)
@@ -14,6 +14,7 @@ function App() {
         <Routes>
           <Route path='/register' element={<Signup />}></Route>
           <Route path='/login' element={<Login />}></Route>
+          <Route path='/home' element={<Home />}></Route>
         </Routes>
       </BrowserRouter>
     </div>
---
+++ Client/src/Home.jsx
@@ -0,0 +1,12 @@
+import {useState} from "react";
+
+function Home(){
+
+    const [,] = useState()
+
+    return(
+        <h2>Welcome to the home page</h2>
+    )
+}
+
+export default Home;
