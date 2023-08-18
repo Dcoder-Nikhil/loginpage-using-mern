86b7bda8d49fb89f5304237b38a7046e281ba895
DcoderLLC <dcoderllc@gmail.com>
2023-08-19 03:18:53 +0530

    login page UI and api integration added

--- server/index.js
+++ server/index.js
@@ -10,6 +10,23 @@ app.use(cors())
 
 mongoose.connect("mongodb://127.0.0.1:27017/employee")
 
+app.post('/login', (req, res) => {
+    const {email, password} = req.body;
+    EmployeeModel.findOne({email: email})
+    .then(user => {
+        if(user){
+            if(user.password === password){
+                res.json("success")
+            } else{
+                res.json("the password is incorrect")
+            }
+        } else{
+            res.json("the user doesn't exists")
+        }
+    })
+})
+
+
 app.post('/registor', (req, res) =>{
     EmployeeModel.create(req.body)
     .then(employees => res.json(employees))
